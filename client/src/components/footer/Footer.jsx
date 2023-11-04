998926e0c6a6cb4fe604071593c5cdc696962526
IordanTyparow <120342477+IordanTyparow@users.noreply.github.com>
2023-11-04 19:42:10 +0200

    add footer icons

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,17 @@
 import "./Footer.css"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
+
 export default function Footer() {
     return (
         <footer>
             <p>&copy; 2023 Your Website Name</p>
-            <p>Designed by <a href="https://www.example.com">Your Name</a></p>
+            <p>Designed by Iordan Typarow</p>
+            <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="#"><FontAwesomeIcon icon={faLinkedin} /></a>
+            <a href="#"><FontAwesomeIcon icon={faGithub} /></a>
         </footer>
     )
 }
