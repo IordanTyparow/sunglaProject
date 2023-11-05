0db3484a38b9b6ac669fc12d6c030ce95d508c79
IordanTyparow <120342477+IordanTyparow@users.noreply.github.com>
2023-11-05 16:22:56 +0200

    fix some css and edit home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,32 +1,19 @@
 import "./Home.css"
 
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
-        <section>
+        <section className="home-page">
             <div className="header">
                 <h1>Home page</h1>
                 <p>Pick your perfect sunglasses for the summer!</p>
-                <p className="glassesMessage">Most bought sunglasses</p>
             </div>
-            <div className="container">
-                <div className="product">
-                    <img src="/images/glass4.png" />
-                    <h3>Brand: product</h3>
-                    <span>Price: $100</span>
-                    <a href="">More info</a>
-                </div>
-                <div className="product">
-                    <img src="/images/glass3.png" />
-                    <h3>Brand: product</h3>
-                    <span>Price: $100</span>
-                    <a href="">More info</a>
-                </div>
-                <div className="product">
-                    <img src="/images/glass3.png" />
-                    <h3>Brand: product</h3>
-                    <span>Price: $100</span>
-                    <a href="">More info</a>
-                </div>
+            <div className="sunglasses-container">
+                <Link to="/catalog" className="sunglasses-link">
+                    <img src="/images/glass3.png" alt="Sunglasses" className="sunglasses-image" />
+                    <span className="ux-message">Browse Sunglasses</span>
+                </Link>
             </div>
         </section >
     );
