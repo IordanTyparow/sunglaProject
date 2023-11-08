5856030ed4a7d5cc7475ac52eabde343f3ee4427
IordanTyparow <120342477+IordanTyparow@users.noreply.github.com>
2023-11-08 22:03:32 +0200

    add details page

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -1,28 +1,35 @@
 import "./Details.css";
 
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 import { AuthContext } from "../../../context/authContext"
+import * as sunglassesService from "../../services/sunglassesService"
 
 export default function Detaitls() {
+    const [current, setCurrent] = useState({});
     const { user } = useContext(AuthContext);
     const { sunglassesId } = useParams();
+
+    useEffect(() => {
+        sunglassesService.getOne(sunglassesId)
+            .then(data => setCurrent(data));
+    }, []);
 
     return (
         <section className="details-page">
             <h1>Details page</h1>
 
             <div className="product">
-                <img src='/images/glass4.png' alt="photo" />
-                <h3>Brand: $test</h3>
-                <p className="price"><strong>Price:</strong > $100</p>
-                <p>Description: some!</p>
+                <img src={current.imageUrl} alt="photo" />
+                <h3>Brand: {current.brand}</h3>
+                <p className="price"><strong>Price:</strong > ${current.price}</p>
+                <p>Description: {current.description}</p>
                 <div className="buttons">
-                    <Link to={`/sunglasses/1/edit`}>Edit</Link>
-                    <Link to={`/sunglasses/1/delete`}>Delete</Link>
-                    <Link to={`/sunglasses/1/buy`}>Buy</Link>
+                    <Link to={`/sunglasses/${current._id}/edit`}>Edit</Link>
+                    <Link to={`/sunglasses/${current._id}/delete`}>Delete</Link>
+                    <Link to={`/sunglasses/${current._id}/buy`}>Buy</Link>
                 </div>
             </div>
         </section >
